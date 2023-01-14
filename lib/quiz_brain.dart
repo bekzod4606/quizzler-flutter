8d812269c01f52bff2c31252d9f85a4c9405c6bb
Bekzod <110982325+bekzod4606@users.noreply.github.com>
2023-01-14 23:24:03 +0100

    changed

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: unused_import, unnecessary_string_escapes, prefer_final_fields, unused_field, empty_statements, no_leading_underscores_for_local_identifiers, avoid_print
+
+import 'question.dart';
+
+class QuizBrain {
+  int _questionNumber = 0;
+  List<Questions> _questionTank = [
+    Questions(q: 'Some cats are actually allergic to humans', a: true),
+    Questions(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Questions(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Questions(q: 'A slug\'s blood is green.', a: true),
+    Questions(q: 'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', a: true),
+    Questions(q: 'It is illegal to pee in the Ocean in Portugal.', a: true),
+    Questions(
+        q: 'No piece of square dry paper can be folded in half more than 7 times.',
+        a: false),
+    Questions(
+        q: 'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        a: true),
+    Questions(
+        q: 'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        a: false),
+    Questions(
+        q: 'The total surface area of two human lungs is approximately 70 square metres.',
+        a: true),
+    Questions(q: 'Google was originally called \"Backrub\".', a: true),
+    Questions(
+        q: 'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        a: true),
+    Questions(
+        q: 'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        a: true),
+  ];
+  void nextQuestion() {
+    if (_questionNumber < _questionTank.length - 1) ;
+    _questionNumber++;
+    print(_questionNumber);
+    print(_questionTank.length);
+  }
+
+  String getQuestionText() {
+    return _questionTank[_questionNumber].questionText;
+  }
+
+  bool getCorrectAnswer() {
+    return _questionTank[_questionNumber].questionAnswer;
+  }
+}
